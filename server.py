02cfca3d6d21a05b84e5556b692ac596d528e8d5
RopeLipe <z1n3xubisoft@gmail.com>
2025-04-26 03:21:06 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -204,8 +204,8 @@ def api_install():
                     boot_size_mib = 512 
                     boot_part = {
                         "status": "create", "type": "primary",
-                        "start": {"unit": "MiB", "value": 1},
-                        "size": {"unit": "MiB", "value": boot_size_mib}, # Fixed size for boot
+                        "start": {"unit": "MiB", "value": 1, "sector_size": None},
+                        "size": {"unit": "MiB", "value": boot_size_mib, "sector_size": None},
                         "fs_type": "fat32",
                         "mountpoint": "/boot",
                         "flags": ["boot", "esp"],
@@ -218,8 +218,8 @@ def api_install():
                     root_start_mib = 1 + boot_size_mib
                     root_part = {
                         "status": "create", "type": "primary",
-                        "start": {"unit": "MiB", "value": root_start_mib},
-                        "size": {"unit": "Percent", "value": root_percentage},
+                        "start": {"unit": "MiB", "value": root_start_mib, "sector_size": None},
+                        "size": {"unit": "Percent", "value": root_percentage, "sector_size": None},
                         "fs_type": filesystem_str,
                         "mountpoint": "/",
                         "flags": [],
@@ -232,7 +232,7 @@ def api_install():
                     home_part = {
                         "status": "create", "type": "primary",
                         # Omitting start lets the partitioner place it after root
-                        "size": {"unit": "Percent", "value": 100}, # 100% of remaining space
+                        "size": {"unit": "Percent", "value": 100, "sector_size": None}, # 100% of remaining space
                         "fs_type": filesystem_str,
                         "mountpoint": "/home",
                         "flags": [],
